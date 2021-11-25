ec36b3e7883d2137c42039039361f2ee08932342
joaotcmeirinho <jmeiradas@gmail.com>
2021-11-25 18:49:19 +0000

    set display radios functionality

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import video from "../../assets/video/backgroundV.mp4";
 import Logo from "../../assets/video/LogoWhite.png";
 import "./style.css";
 import RadioFmForm from "../RadioFmForm/RadioFmForm";
+import { RadioDisplay } from "../RadioDisplay/RadioDisplay";
 
 const MainPage = () => {
   return (
---
+++ src/components/RadioDisplay/RadioDisplay.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const RadioDisplay = () => {
+  return <div>RadioDisplay</div>;
+};
