af79d9e43bca1dd6b40e57574870e4d423d391e3
hugosylva <hugosylva@hotmail.com>
2021-11-25 21:58:18 +0000

    Form and styling on main page update

--- src/components/RadioFmForm/RadioFmForm.js
+++ src/components/RadioFmForm/RadioFmForm.js
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { RadioDisplay } from "../RadioDisplay/RadioDisplay";
+import "./style.css"
 
 export default function RadioFmForm() {
   const [countryRadioStations, setCountryRadioStations] = useState("");
@@ -44,31 +45,27 @@ export default function RadioFmForm() {
   };
   console.log("hello");
 
-  return (
+  return !radioDisplay ? (
     <>
-      {!radioDisplay ? (
-        <div>
-          <h1>form</h1>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <input
+          <div className="blur-box"></div>
+        <div className="form-handler">
+          <form className="form-style" onSubmit={handleSubmit(onSubmit)}>
+            <input className="country-input"
               type="text"
               placeholder="Country"
               {...register("Country", {})}
-            />
+              />
             <select {...register("genre")}>
               {genre
                 .filter((item, index) => genre.indexOf(item) === index)
-                .map((g) => (
-                  <option value={g}>{g}</option>
-                ))}
+                .map((g) => <option value={g}>{g}</option>)}
             </select>
-
             <input type="submit" />
           </form>
-        </div>
+                </div>
+         
+        </>
       ) : (
         <div>hello</div>
       )}
-    </>
-  );
-}
+
