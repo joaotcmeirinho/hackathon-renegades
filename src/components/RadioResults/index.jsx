a0bdfe632831f4e2277919d0ddc81d689af64d76
Paulo <paulocpc2202@gmail.com>
2021-11-26 10:44:58 +0000

    accordion fixed

--- src/components/RadioResults/index.jsx
+++ src/components/RadioResults/index.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "./style.css"
 import image from '../../assets/video/Dashboard.png'
-import { Accordion } from 'react-bootstrap'
+import { Accordion, Card } from 'react-bootstrap'
 
 const RadioResults = ({toggleDisplay}) => {
     return (
         <>
-         <button onClick={toggleDisplay}>Back</button>
+         
         <div className="">
          <img className="driver-img" src={image} alt="" 
          style={{  position: "absolute",
@@ -17,18 +17,22 @@ const RadioResults = ({toggleDisplay}) => {
          height:"70vh", 
          transform: "translate(-50%, -50%)", 
              }} />
-        <Accordion defaultActiveKey="0" flush>
-            <Accordion.Item eventKey="1">
-            <Accordion.Button centered><h4>title<hr/>country</h4></Accordion.Button>
             
-            <Accordion.Body>
-      <div className="div">hashdhashd</div>
-      <div className="div">hashdhashd</div>
-      <div className="div">hashdhashd</div>
+            <Accordion defaultActiveKey="0" flush>
+            <Accordion.Item eventKey="0">
+            <Accordion.Body as={Card.Body}>
+             <div className="">asdasdasdasd</div>
+             <div className="">asdasdasdasd</div>
+             <div className="">asdasdasdasd</div>
             </Accordion.Body>
+            <Accordion.Button centered>
+                <h4>title<hr/>country</h4>
+            </Accordion.Button>
+        
             
             </Accordion.Item>
             </Accordion>
+            <button onClick={toggleDisplay} className="button-style">Back</button>
         </div>
             
         </>
